7668abe471e9ac53b99d28ac6c1013082d63f200
August Cayzer <a@cyzr.me>
2026-08-27 10:32:09 +0100

    chore: sessionmail 0.2.0

--- Formula/sessionmail.rb
+++ Formula/sessionmail.rb
@@ -1,8 +1,8 @@
 class Sessionmail < Formula
   desc "Local, paired mailbox for AI coding agents that can't otherwise reach each other"
   homepage "https://github.com/aicayzer/sessionmail"
-  url "https://registry.npmjs.org/sessionmail/-/sessionmail-0.1.0.tgz"
-  sha256 "09c221eabbf63d4b1cca2b31d4f8fe99fa031901e20c6e3d382f29036f3b0b4c"
+  url "https://registry.npmjs.org/sessionmail/-/sessionmail-0.2.0.tgz"
+  sha256 "d8ff00dfdb977e50d05c68b378c63a2737961a787548d13f185784d19d17e248"
   license "MIT"
   head "https://github.com/aicayzer/sessionmail.git", branch: "main"
 
@@ -14,6 +14,6 @@ class Sessionmail < Formula
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/sessionmail --version")
+    assert_match "0.2.0", shell_output("#{bin}/sessionmail --version")
   end
 end
